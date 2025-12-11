b9665bd1f86232a19a59c3297144b1395c0567e2
Lance Schweiger <95454812+lanceschweiger@users.noreply.github.com>
2025-12-10 18:23:50 -0700

    Update server.c

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,68 +8,55 @@
 #include <sys/stat.h>
 
 struct message {
-	char source[50];
-	char target[50]; 
-	char msg[200]; // message body
+    char source[50];
+    char target[50];
+    char msg[200];
 };
 
 void terminate(int sig) {
-	printf("Exiting....\n");
-	fflush(stdout);
-	exit(0);
+    printf("Exiting....\n");
+    fflush(stdout);
+    exit(0);
 }
 
 int main() {
-	int server;
-	int target;
-	int targetfd;
-	int dummyfd;
-	struct message req;
-	ssize_t n;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
+    int server = -1;
+    int targetfd = -1;
+    int dummyfd = -1;
+    struct message req;
+    ssize_t n;
 
-	if (mkfifo("serverFIFO", 0666) < 0) {
-		if (errno != EEXIST) {
-			fprintf(stderr, "server: mkfifo failed: %s\n", strerror(errno));
-			return EXIT_FAILURE;
-		}
-	}
-	
-	server = open("serverFIFO",O_RDONLY | O_NONBLOCK);
-	if (server < 0) {
-		fprintf(stderr, "server: cannot open serverFIFO for reading: %s\n", strerror(errno));
-		dummyfd = open("serverFIFO",O_WRONLY | O_NONBLOCK);
-		return EXIT_FAILURE;
-	}
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, terminate);
 
-	dummyfd = open("serverFIFO",O_WRONLY | O_NONBLOCK);
+    if (mkfifo("serverFIFO", 0666) < 0 && errno != EEXIST) {
+        fprintf(stderr, "server: mkfifo failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
 
-	while (1) {
+    server = open("serverFIFO", O_RDONLY | O_NONBLOCK);
+    if (server < 0) {
+        fprintf(stderr, "server: cannot open serverFIFO for reading: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    dummyfd = open("serverFIFO", O_WRONLY | O_NONBLOCK);
+
+    while (1) {
         n = read(server, &req, sizeof(req));
         if (n == sizeof(req)) {
             printf("Received a request from %s to send the message %s to %s.\n",
                    req.source, req.msg, req.target);
             fflush(stdout);
 
-            targetfd = open(req.target, O_WRONLY | O_NONBLOCK);
-			if (targetfd < 0) {
-    			if (errno == ENXIO) {
-        			continue;
-    			} else {
-        			fprintf(stderr, "server: failed to open target FIFO %s: %s\n",
-                	req.target, strerror(errno));
-        			continue;
-    			}
-			}
-
-            if (write(targetfd, &req, sizeof(req)) != sizeof(req)) {
-                fprintf(stderr, "server: write to %s failed: %s\n",
-                        req.target, strerror(errno));
+            targetfd = open(req.target, O_WRONLY);
+            if (targetfd < 0) {
+                perror("server: failed to open target FIFO");
+                continue;
             }
 
+            dprintf(targetfd, "Incoming message from %s: %s\n", req.source, req.msg);
             close(targetfd);
-            targetfd = -1;
             continue;
         } else if (n == 0) {
             usleep(200000);
