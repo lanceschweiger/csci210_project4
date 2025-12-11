748b4e507c1f7978f4605ce9d654f92fe3ac2703
Lance Schweiger <95454812+lanceschweiger@users.noreply.github.com>
2025-12-10 18:15:54 -0700

    Update server.c

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 struct message {
 	char source[50];
@@ -28,46 +29,65 @@ int main() {
 	ssize_t n;
 	signal(SIGPIPE,SIG_IGN);
 	signal(SIGINT,terminate);
+
+	if (mkfifo("serverFIFO", 0666) < 0) {
+		if (errno != EEXIST) {
+			fprintf(stderr, "server: mkfifo failed: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+	}
 	
-	server = open("serverFIFO",O_RDONLY);
+	server = open("serverFIFO",O_RDONLY | O_NONBLOCK);
 	if (server < 0) {
 		fprintf(stderr, "server: cannot open serverFIFO for reading: %s\n", strerror(errno));
+		dummyfd = open("serverFIFO",O_WRONLY | O_NONBLOCK);
+		return EXIT_FAILURE;
 	}
-	dummyfd = open("serverFIFO",O_WRONLY);
+
+	dummyfd = open("serverFIFO",O_WRONLY | O_NONBLOCK);
 
 	while (1) {
-		// TODO:
-		// read requests from serverFIFO
+        n = read(server, &req, sizeof(req));
+        if (n == sizeof(req)) {
+            printf("Received a request from %s to send the message %s to %s.\n",
+                   req.source, req.msg, req.target);
+            fflush(stdout);
 
-		n = read(server, &req, sizeof(req));
-		if (n <= 0) {
-			if (n == 0) {
-				usleep(200000);
-				continue;
-			}
-			if (n == -1) {
-				usleep(200000);
-				continue;
-			}
-		}
-		
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-		fflush(stdout);
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-		targetfd = open(req.target, O_WRONLY);
-		if (targetfd < 0) {
-			fprintf(stderr, "server: failed to open target FIFO %s: %s\n", req.target, strerror(errno));
-			continue;
-		}
-		if (write(targetfd, &req, sizeof(req)) != sizeof(req)) {
-			fprintf(stderr, "server: write to %s failed: %s\n", req.target, strerror(errno));
-		}
-		close(targetfd);
-	}
-	close(server);
-	if (dummyfd >= 0) close(dummyfd);
-	return 0;
+            targetfd = open(req.target, O_WRONLY | O_NONBLOCK);
+            if (targetfd < 0) {
+                fprintf(stderr, "server: failed to open target FIFO %s: %s\n",
+                        req.target, strerror(errno));
+                continue;
+            }
+
+            if (write(targetfd, &req, sizeof(req)) != sizeof(req)) {
+                fprintf(stderr, "server: write to %s failed: %s\n",
+                        req.target, strerror(errno));
+            }
+
+            close(targetfd);
+            targetfd = -1;
+            continue;
+        } else if (n == 0) {
+            usleep(200000);
+            continue;
+        } else if (n == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                usleep(200000);
+                continue;
+            } else {
+                fprintf(stderr, "server: read error: %s\n", strerror(errno));
+                usleep(200000);
+                continue;
+            }
+        } else {
+            usleep(200000);
+            continue;
+        }
+    }
+
+    if (server >= 0) close(server);
+    if (dummyfd >= 0) close(dummyfd);
+    return 0;
 }
 
